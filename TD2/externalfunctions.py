7db1fbfb9c86c3abad4369489b8d4224f900f2c4
Vrock691 <39530176+Vrock691@users.noreply.github.com>
2024-04-09 16:23:00 +0000

    Modification des fonctions externes

--- TD2/externalfunctions.py
+++ TD2/externalfunctions.py
@@ -2,27 +2,46 @@ import re
 
 # Fonction de calculs
 def Sum(prompt): 
-    matches = re.search('[0-9].', prompt)
-    print(matches.group(0))
-    n1 = matches[0]
-    n2 = matches[1]
-    return "> " + (n1 + n2) + ""
+    matches = re.findall('([0-9]+)', prompt)
+    if len(matches) >= 2:
+        n1 = int(matches[0])
+        n2 = int(matches[1])
+        sum = n1+n2
+        return "> " + str(n1) + " + " + str(n2) + " = " + str(sum)
+    else:
+        return "Il me semble qu'il manque un nombre pour faire cet addition"
     
 def Divide(prompt):
-    if n1 and n2!=0:
-        return n1/n2
+    matches = re.findall('([0-9]+)', prompt)
+    if len(matches) >= 2:
+        n1 = int(matches[0])
+        n2 = int(matches[1])
+        if n1 and n2!=0:
+            return n1/n2
+        else:
+            return "Division par 0 impossible !"
     else:
-        return "Division par 0 impossible !"
+        return "Division impossible, chiffre manquant"
 
 def Multiplication(prompt):
-    n1 = int(input("Entrez le premier nombre : "))
-    n2 = int(input("Entrez le deuxième nombre : "))
-    return n1 * n2
+    matches = re.findall('([0-9]+)', prompt)
+    if len(matches) >= 2:
+        n1 = int(matches[0])
+        n2 = int(matches[1])
+        prod = n1*n2
+        return "> " + str(n1) + " x " + str(n2) + " = " + str(prod)
+    else:
+        return "Il me semble qu'il manque un nombre pour faire cette multiplication"
 
 def Soustraction(prompt):
-    n1 = int(input("Entrez le premier nombre : "))
-    n2 = int(input("Entrez le deuxième nombre : "))
-    return n1 - n2
+    matches = re.findall('([0-9]+)', prompt)
+    if len(matches) >= 2:
+        n1 = int(matches[0])
+        n2 = int(matches[1])
+        dif = n1-n2
+        return "> " + str(n1) + " - " + str(n2) + " = " + str(dif)
+    else:
+        return "Il me semble qu'il manque un nombre pour faire cet addition"
 
 def RecordNewResponse(prompt):
     return "Enregistré"
